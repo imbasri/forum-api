be4db7b8056fd082edefa6ad49e7a64d321fe812
imbasri <imam.basri@edi-indonesia.co.id>
2024-08-19 14:02:43 +0700

    setup 7 | Membuat Model Thread Dan Relasinya

--- app/Models/Category.ts
+++ app/Models/Category.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import Thread from './Thread'
 
 export default class Category extends BaseModel {
   @column({ isPrimary: true })
@@ -13,4 +14,9 @@ export default class Category extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  // one to many
+  @hasMany(() => Thread)
+  public threads: HasMany<typeof Thread>
+
 }
